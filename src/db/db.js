ddaea4d2df2dee0c058ec3988014f90d48fc6c85
allanheil1 <allanheil1@gmail.com>
2023-01-23 15:07:59 -0300

    DATABASE_URL variable changed

--- src/db/db.js
+++ src/db/db.js
@@ -4,7 +4,7 @@ import { MongoClient } from 'mongodb';
 dotenv.config();
 let db;
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
 
@@ -16,7 +16,7 @@ try {
 
 }
 
-db = mongoClient.db('dbMyWallet');
+db = mongoClient.db();
 
 export default db;
 
